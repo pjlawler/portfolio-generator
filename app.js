b671a33a51f3ea42d31541afc6da20816c7e0ed7
Pat Lawler <patlaw777@icloud.com>
2022-02-11 20:17:29 -0600

    added questions

--- app.js
+++ app.js
@@ -11,22 +11,46 @@ const inquirer = require('inquirer');
 //     console.log("Portfolio complete! Check out index.html to see the output!");
 // });
 
+const verifyAnswer = (answer, message) => {
+        if(!answer) { 
+            console.log(message);
+            return false;
+        } else {
+            return true;
+        }
+};
+
 const promptUser = () => {
     return inquirer.prompt([
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: answer => verifyAnswer(answer, "Please enter your name")
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: answer => verifyAnswer(answer, "Please enter your GitHub Username")
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
       },
       {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+            if (confirmAbout) {
+                return true;
+            } else {
+                return false;
+            }
+        }
       }
     ]);
   };
@@ -51,12 +75,14 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project. (Required)'
+            message: 'Provide a description of the project. (Required)',
+            validate: answer => verifyAnswer(answer, "Please a project description")
         },
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: answer => verifyAnswer(answer, "Please enter the GitHub link")
         },
         {
             type: 'checkbox',
